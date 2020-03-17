936574c334690b985827d0c6ff51a4e7ae85957b
Damian Warszawski <damian.warszawski@ing.com>
2020-03-17 17:13:16 +0100

    change order of init scripts

--- amundsenatlastypes/types_def.py
+++ amundsenatlastypes/types_def.py
@@ -5,10 +5,10 @@ def get_schema(schema):
     return pkg_resources.resource_string(__name__, schema).decode('utf-8')
 
 
-table_schema = get_schema("schema/01_table_schema.json")
-column_schema = get_schema("schema/01_1_column_schema.json")
+table_schema = get_schema("schema/01_2_table_schema.json")
+column_schema = get_schema("schema/01_column_schema.json")
 user_schema = get_schema("schema/02_user.json")
-reader_schema = get_schema("schema/03_reader.json")
+reader_schema = get_schema("schema/01_1_reader.json")
 user_reader_relation = get_schema("schema/04_user_reader_relation.json")
 table_partition_schema = get_schema("schema/05_table_partition_schema.json")
 hive_table_partition = get_schema("schema/05_1_hive_table_partition.json")
